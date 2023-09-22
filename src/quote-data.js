b1fcab6d6f1a81bac8fe0e8a043721e9e5cf4b9d
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-22 19:30:52 -0400

    added quote-data with data from app.js

---
+++ src/quote-data.js
@@ -0,0 +1,55 @@
+const quoteData = [
+  {
+    quote:
+      'If you really look closely, most overnight successes took a long time',
+    author: ' Steve Jobs',
+  },
+  {
+    quote: 'Life is what happens when you are busy making other plans',
+    author: 'John Lennon',
+  },
+  {
+    quote:
+      'It is during our darkest moments that we must focus to see the light',
+    author: 'Aristotle',
+  },
+  {
+    quote:
+      'He who is not courageous enough to take risks will accomplish nothing in life',
+    author: 'Muhammad Ali',
+  },
+  {
+    quote: 'The only thing we have to fear is fear itself',
+    author: 'Franklin Delano Roosevelt',
+  },
+  {
+    quote:
+      'The only thing necessary for the triumph of evil is for good men to do nothing',
+    author: 'Edmund Burke',
+  },
+  {
+    quote: 'To be or not to be, that is the question',
+    author: 'William Shakespeare',
+  },
+  {
+    quote:
+      'Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that',
+    author: 'Martin Luther King',
+  },
+  { quote: 'The unexamined life is not worth living', author: 'Socrates' },
+  {
+    quote:
+      'Ask not what your country can do for you, ask what you can do for your country',
+    author: 'John F. Kennedy',
+  },
+  {
+    quote: 'Be the change that you wish to see in the world',
+    author: 'Mahatma Gandhi',
+  },
+  {
+    quote: 'The only source of knowledge is experience',
+    author: 'Albert Einstein',
+  },
+];
+
+export default quoteData;
