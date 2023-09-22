fee1a49c7267e2189fbc68f4046febca1d6d772c
Orlando Alvarez <oalvarezalarcon0304@gmail.com>
2023-09-22 19:26:02 -0400

    added useState and handler function to button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
 
 const quoteData = [
@@ -55,16 +58,30 @@ const quoteData = [
 ];
 
 function App() {
+  const [quoteInfo, setquoteInfo] = useState(quoteData[0]);
+
+  const changeQuoteHandler = () => {
+    let randomNumber = Math.floor(Math.random() * quoteData.length);
+    let randomQuote = quoteData[randomNumber];
+
+    setquoteInfo(randomQuote);
+  };
+
   return (
     <div className="App">
       <div id="quote-box">
-        <p id="text">text</p>
-        <span id="author">author</span>
+        <p id="text">{quoteInfo.quote}</p>
+        <span id="author">- {quoteInfo.author}</span>
         <div className="click-actions">
-          <button id="new-quote">
+          <button id="new-quote" onClick={changeQuoteHandler}>
             New Quote
           </button>
-          <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">icon</a>
+          <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
+            <FontAwesomeIcon
+              style={{ fontSize: '2.2rem', color: '#3F72AF' }}
+              icon={faTwitter}
+            />
+          </a>
         </div>
       </div>
     </div>
